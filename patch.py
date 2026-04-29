16c448e2634a78abb7794749489cc26891942ac0
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-04-28 21:15:09 -0500

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- patch.py
+++ patch.py
@@ -194,5 +194,6 @@ sudo apt update
 sudo apt install firefox
 echo -e "[Unit]\\nDescription=Firefox Kiosk\\nAfter=network-online.target\\n\\n[Service]\nExecStart=/usr/bin/firefox --kiosk https://v3.windows93.net\\nUser=$USER\\nEnvironment=DISPLAY=:0\\n\\n[Install]\\nWantedBy=graphical.target" | sudo tee /etc/systemd/system/firefox-kiosk.service > /dev/null && sudo systemctl enable firefox-kiosk.service
 """)
+print("That last one will take a few seconds. Don't mind...")
 print("After you've done that, start the Cubic ISO creation process and Vindows93 should be good to go.")
 print("After login, copy .config/i3-plz-add-after-boot's contents to the end of your .config/i3.")
